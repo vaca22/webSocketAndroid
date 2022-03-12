0e84ba0078aaafd3072874c412a9ebabb395c813
vaca22 <wsmslgh@gmail.com>
2022-03-12 20:57:49 +0800

    wetrfwe

--- app/src/main/java/com/viatom/myapplication/JWebSocketClient.java
+++ app/src/main/java/com/viatom/myapplication/JWebSocketClient.java
@@ -2,9 +2,14 @@ package com.viatom.myapplication;
 
 import android.util.Log;
 
+import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
 import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.exceptions.InvalidDataException;
+import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.handshake.ServerHandshake;
+import org.java_websocket.handshake.ServerHandshakeBuilder;
 
 import java.net.URI;
 
@@ -25,11 +30,24 @@ public class JWebSocketClient extends WebSocketClient {
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        Log.e("JWebSocketClient", "onClose()");
+        Log.e("JWebSocketClient", "onClose()"+reason);
+    }
+
+    @Override
+    public void onWebsocketHandshakeSentAsClient(WebSocket conn, ClientHandshake request) throws InvalidDataException {
+      //  super.onWebsocketHandshakeSentAsClient(conn, request);
+    }
+
+
+
+    @Override
+    public void onWebsocketHandshakeReceivedAsClient(WebSocket conn, ClientHandshake request, ServerHandshake response) throws InvalidDataException {
+        super.onWebsocketHandshakeReceivedAsClient(conn, request, response);
     }
 
     @Override
     public void onError(Exception ex) {
         Log.e("JWebSocketClient", "onError()");
+        ex.printStackTrace();
     }
 }
--- app/src/main/java/com/viatom/myapplication/MainActivity.kt
+++ app/src/main/java/com/viatom/myapplication/MainActivity.kt
@@ -1,30 +1,68 @@
 package com.viatom.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
 import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val mHandler_beat = Handler()
+    private val heartBeatRunnable_beat: Runnable = object : Runnable {
+        override fun run() {
+
+
+            //定时对长连接进行心跳检测
+            mHandler_beat.postDelayed(
+                this,
+                1000
+            )
+        }
+    }
+    lateinit var client: JWebSocketClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread{
-            val uri: URI = URI.create("ws://192.168.5.101:8080/user")
-            val client: JWebSocketClient = object : JWebSocketClient(uri) {
+        val myid = "8898"
+        val toid = "889865"
+        Thread {
+            val uri: URI = URI.create("ws://192.168.6.110:8080?fruck=" + myid)
+            client = object : JWebSocketClient(uri) {
                 override fun onMessage(message: String) {
                     //message就是接收到的消息
                     Log.e("JWebSClientService", message)
                 }
             }
             try {
+                client.connectionLostTimeout = 10
                 client.connectBlocking()
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
             if (client != null && client.isOpen()) {
-                client.send("你好");
+                val gaga = JSONObject()
+                gaga.put("fuck", "1123")
+                //  client.send(gaga.toString());
             }
+            repeat(100) {
+                val gaga = JSONObject()
+                gaga.put("id", myid)
+                gaga.put("toid", toid)
+
+                gaga.put("params", "ddfg")
+                client.send(gaga.toString())
+                Thread.sleep(1000)
+            }
+            //client.closeBlocking()
         }.start()
+    }
+
+    override fun onDestroy() {
+        client.closeBlocking()
+        super.onDestroy()
     }
 }
